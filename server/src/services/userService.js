64c9f3956b180bde4623f749252e5d2d8bc4d237
Aqeel <aqeelhanslo@gmail.com>
2024-06-07 08:58:09 +0200

    Implemented admin crud fuctions for users

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -54,14 +54,121 @@ module.exports.UserLoginService = async (username, email, password) => {
             });
         
             if (!user) 
-                throw new Error('User not found');
+                throw new Error('User not found')
         
             const validPassword = await bcrypt.compare(password, user.password);
             if (!validPassword) 
-                throw new Error('Invalid password');
+                throw new Error('Invalid password')
             
             const token = jwt.sign({ _id: user._id, userType: user.userType }, jwtSecret);
-            resolve(token);
+            resolve(token)
+        } catch (error) {
+            reject(error)
+        }
+    });
+}
+
+// READ USERS (VERIFY AUTHORIY)
+module.exports.FindUserByIdService = async (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findById(id)
+            if (!user) {
+                throw new Error('User not found')
+            }
+            resolve(user)
+        }
+        catch (error) {
+            reject (error)
+        }
+    })
+}
+module.exports.FindUserByTokenService = async (token) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            const user = await User.findById(decoded._id)
+            if (!user) {
+                throw new Error('User not found')
+            }
+            resolve(user)
+        }
+        catch (error) {
+            reject (error)
+        }
+    })
+}
+
+// ADMIN USER SERVICES
+module.exports.CreateUserService = async (userDetails) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const existingUser = await User.findOne({ username: userDetails.username });
+            if (existingUser) {
+                return reject('Username already exists');
+            }
+
+            const userModelData = new User();
+            const hashedPassword = await bcrypt.hash(userDetails.password, 10);
+
+            userModelData.firstName = userDetails.firstName;
+            userModelData.lastName = userDetails.lastName;
+            userModelData.email = userDetails.email;
+            // location not included in initial registration
+            userModelData.username = userDetails.username;
+            userModelData.password = hashedPassword;
+            userModelData.userType = userDetails.userType;
+            userModelData.dateCreated = new Date();
+
+            userModelData.save()
+                .then((result) => {
+                    resolve(true);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+module.exports.FindAllUsersService = async () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const users = await User.find({})
+            if (!users) {
+                throw new Error('No users found')
+            }
+            resolve(users)
+        }
+        catch (error) {
+            reject (error)
+        }
+    })
+}
+module.exports.UpdateUserService = async (id, userDetails) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (userDetails.password) {
+                userDetails.password = await bcrypt.hash(userDetails.password, 10);
+            }
+            const user = await User.findByIdAndUpdate(id, userDetails, { new: true });
+            if (!user) {
+                throw new Error('User not found');
+            }
+            resolve(user);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+module.exports.DeleteUserService = async (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findByIdAndDelete(id);
+            if (!user) 
+                throw new Error('User not found');
+            resolve(true);
         } catch (error) {
             reject(error);
         }
