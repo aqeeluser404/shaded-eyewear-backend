627e54b7f92cf64e3e42a579e620d3328695fc3a
aqeeluser404 <aqeelhanslo@gmail.com>
2024-12-17 15:19:25 +0200

    updated mongoose env

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 exports.connect = function() {
     mongoose.connect(
-        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.czmy3sa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=/${process.env.DB_CLUSTER}`
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}${process.env.DB_CLUSTER_LINE_1}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=/${process.env.DB_CLUSTER_LINE_2}`
     )
     const db = mongoose.connection;
     db.on('error', (error) => {
